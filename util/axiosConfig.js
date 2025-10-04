31c4b6d41408a12c8829386f0b606a3c81c557f5
Ahmed El-Messery <78704916+ahmedelmessery@users.noreply.github.com>
2025-10-04 23:32:54 +0300

    Update axiosConfig.js

--- util/axiosConfig.js
+++ util/axiosConfig.js
@@ -1,22 +1,22 @@
 // // utils/axiosConfig.js
 
-// import axios from 'axios';
+import axios from 'axios';
 
-// const axiosInstance = axios.create({
-// baseURL: 'https://elaf.onrender.com',
-// });
+ const axiosInstance = axios.create({
+ baseURL: 'https://elaf.onrender.com',
+ });
 
-// axiosInstance.interceptors.request.use(
-//   function (config) {
-//     const token = localStorage.getItem('token');
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   function (error) {
-//     return Promise.reject(error);
-//   }
-// );
+ axiosInstance.interceptors.request.use(
+   function (config) {
+     const token = localStorage.getItem('token');
+     if (token) {
+       config.headers['Authorization'] = `Bearer ${token}`;
+     }
+     return config;
+   },
+   function (error) {
+     return Promise.reject(error);
+   }
+ );
 
-// export default axiosInstance;
+ export default axiosInstance;
